0e4291b28f0fa79d26fb21b92b8e28660feeaf20
Dinesh H Naik <dinesh.naik@dreamorbit.com>
2022-03-09 02:38:00 +0530

    added elements

--- frontapp.js
+++ frontapp.js
@@ -6,15 +6,61 @@ Front.contextUpdates.subscribe(context => {
     switch(context.type) {
       case 'noConversation':
         console.log('No conversation selected');
+        this.resetValues();
         break;
       case 'singleConversation':
         console.log('Selected conversation:', context.conversation);
+        this.displayConversationInfo(context, 'singleConversation');
         break;
       case 'multiConversations':
         console.log('Multiple conversations selected', context.conversations);
+        this.displayConversationInfo(context, 'multiConversations');
         break;
       default:
         console.error(`Unsupported context type: ${context.type}`);
+        this.resetValues();
         break;
     }
   });
+
+function resetValues()
+{
+   const sender = document.getElementById('sender');
+   const teammate = document.getElementById('teammate');
+   sender.textContent = '';
+   teammate.textContent = '';
+}
+
+function displayConversationInfo(context, contextType)
+{
+  const sender = document.getElementById('sender');
+  const teammate = document.getElementById('teammate'); //assignee
+  // const subject = document.getElementById('subject');
+  const status = document.getElementById('convstatus'); //status
+  const conversation = null;
+  sender.textContent = '';
+  teammate.textContent = context.teammate.name;
+  sender.textContent = context.teammate.name;
+  
+  if(contextType === 'multiConversations')
+  {
+      conversation = context.conversations[0];
+  }
+  else
+  {
+    conversation = context.conversation;
+  }
+  status.textContent = conversation.status;
+  subject.textContent = conversation.subject;
+
+}
+
+function tagConversation()
+{
+
+}
+
+function untagConversation()
+{
+
+}
